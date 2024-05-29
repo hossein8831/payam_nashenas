8127565cc2d02b9ad9c950c28090d1aab8c2ac19
Miraaaaaleee <115214704+hossein8831@users.noreply.github.com>
2024-05-29 20:16:58 +0330

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
     alert('لطفا یه چیزی بنویس');
     return;
     }
-    fetch(`https://eitaayar.ir/api/api_key/sendMessage?chat_id=9315167&text=${topic}&date=0&parse_mode=&pin=off&viewCountForDelete=`)
+    fetch(`https://eitaayar.ir/api/bot31381:4b95090c-e666-44dd-ac63-ae0cf5855975/sendMessage?chat_id=9315167&text=${topic}&date=0&parse_mode=&pin=off&viewCountForDelete=`)
 })
 })
